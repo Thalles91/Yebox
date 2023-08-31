fb91f65ea9a93294ccb8a5c65104653b6650e6eb
Thalles Gonçalves de Oliveira <SOL0013984@aluno.firjansenaisesi.com.br>
2023-08-31 12:05:37 -0300

    criação de projeto

--- js/main.js
+++ js/main.js
@@ -10,6 +10,16 @@ let Start = setInterval(()=>{
     // Add your methods for Sanddox here
     YeEngine.AlterThemeSystem(YeEngine.Theme.DarkTheme)
     YeEngine.AlterTheme(YeEngine.Theme.TemaCinza)
+    YeEngine.ModifyW(YeEngine.getElementById("card"),"90vh")
+    YeEngine.ModifyH(YeEngine.getElementById("card"),"60vh")
     //End Your methods Here
     clearInterval(Start) //Dont erase this line
 },400)
+
+let update = setInterval(()=>{
+    if(YeEngine.Questions_save[0] !=undefined)
+    {
+        YeEngine.ModifyContentClass(document.getElementById("quest1"),YeEngine.Questions_save[0])
+        clearInterval(update)
+    }
+},600)
