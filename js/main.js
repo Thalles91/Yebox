dce375eec230f65eb70465fd967cc421cdf97427
Thalles Gonçalves de Oliveira <SOL0013984@aluno.firjansenaisesi.com.br>
2023-08-31 11:18:43 -0300

    criação de tema

--- js/main.js
+++ js/main.js
@@ -8,7 +8,8 @@ YeEngine.CreateBasicTags()
 
 let Start = setInterval(()=>{
     // Add your methods for Sanddox here
-
+    YeEngine.AlterThemeSystem(YeEngine.Theme.DarkTheme)
+    YeEngine.AlterTheme(YeEngine.Theme.TemaCinza)
     //End Your methods Here
     clearInterval(Start) //Dont erase this line
 },400)
